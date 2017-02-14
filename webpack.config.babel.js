b1494f73d61abcdc077bb001b901a7c4e1404994
Dani Kenan <danikenan@gmail.com>
2017-02-14 09:03:25 +0200

    1.0.3 - minimization and build improvements

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,18 +1,63 @@
-export default {
-  entry: [
-    './src',
-  ],
-  output: {
-    filename: 'redux-toolbelt.js',
-    path: './dist',
-    library: 'ReduxToolbelt',
-    libraryTarget: 'umd',
-  },
-  module: {
-    rules: [
-      {test: /\.js$/, exclude: /node_modules/, loader: 'eslint-loader', enforce: 'pre'},
-      {test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'},
-    ],
-  },
-}
+import webpack from 'webpack'
+import merge from 'webpack-merge'
 
+export default (env) => {
+
+  process.env.BABEL_ENV = process.env.NODE_ENV = env.build
+
+  const prod = {
+    entry: [
+      './src',
+    ],
+    output: {
+      filename: 'redux-toolbelt.js',
+      path: './dist',
+      library: 'ReduxToolbelt',
+      libraryTarget: 'umd',
+    },
+    module: {
+      rules: [{
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'eslint-loader',
+          enforce: 'pre',
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+        },
+      ],
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env': {
+          'NODE_ENV': JSON.stringify('production'),
+        },
+      }),
+    ],
+  }
+
+
+  const min = {
+    output: {
+      filename: 'redux-toolbelt.min.js',
+    },
+    plugins: [
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          warnings: false,
+          drop_console: true,
+        },
+        mangle: {
+          screw_ie8 : true,
+        },
+      }),
+    ],
+  }
+
+  return [
+    prod,
+    merge(prod, min),
+  ]
+}
