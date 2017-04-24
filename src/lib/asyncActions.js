c6c09fde62a7412dfc6897695c7a9151170cbba7
Dani Kenan <danikenan@gmail.com>
2017-04-24 06:49:08 +0300

    add no/short/full mapping and withDefaults to action creators

--- src/lib/asyncActions.js
+++ src/lib/asyncActions.js
@@ -1,29 +1,66 @@
-export const ACTION_PREFIX = process.env.ACTION_PREFIX || ''
-export const ACTION_ASYNC_REQUEST_SUFFIX = process.env.ACTION_ASYNC_REQUEST_SUFFIX || '@ASYNC_REQUEST'
-export const ACTION_ASYNC_SUCCESS_SUFFIX = process.env.ACTION_ASYNC_SUCCESS_SUFFIX || '@ASYNC_SUCCESS'
-export const ACTION_ASYNC_FAILURE_SUFFIX = process.env.ACTION_ASYNC_FAILURE_SUFFIX || '@ASYNC_FAILURE'
 
+const ACTION_PREFIX = ''
+
+export const ACTION_ASYNC_REQUEST_SUFFIX = '@ASYNC_REQUEST'
+export const ACTION_ASYNC_SUCCESS_SUFFIX = '@ASYNC_SUCCESS'
+export const ACTION_ASYNC_FAILURE_SUFFIX = '@ASYNC_FAILURE'
+export const ACTION_ASYNC_SUCCESS_METHOD = 'success'
+export const ACTION_ASYNC_FAILURE_METHOD = 'failure'
+
+
+const trivialArgsMapper = (payload, meta) => ({payload, meta})
 /**
  *
  * @param name
- * @param options
+ * @param argsMapper
+ * @param options: {prefix, defaultMeta}
  * @returns {function(*, *): {type: string, payload: *, meta: *}}
  */
-export function makeActionCreator(name, options) {
+export function makeActionCreator(name, argsMapper = trivialArgsMapper, options) {
 
-  const defaults = {defaultMeta: undefined, prefix: ACTION_PREFIX}
+  const defaults = {
+    prefix: ACTION_PREFIX,
+    defaultMeta: undefined,
+  }
+
   options = Object.assign(defaults, options)
 
   const type = `${options.prefix}${name}`
-  const actionCreator = (payload, meta) => ({
-    type,
-    payload,
-    meta: options.defaultMeta? Object.assign({}, options.defaultMeta, meta) : meta,
-  })
+
+  const actionCreator = (...args) => {
+    const mapped = argsMapper(...args)
+    let payload = mapped
+    let meta = undefined
+    if(mapped){
+      const hasPayload = Reflect.has(mapped, 'payload')
+      const hasMeta = Reflect.has(mapped, 'meta')
+      const hasMore = Reflect.ownKeys(mapped).length > 2
+      if(hasPayload !== hasMeta || hasPayload && hasMeta && hasMore){
+        throw new Error('Full mapper should return both meta and payload, and only these two.')
+      }
+      if(hasPayload){
+        payload = mapped.payload
+        meta = mapped.meta
+      }
+    }
+    return {
+      type,
+      payload,
+      meta: options.defaultMeta? Object.assign({}, options.defaultMeta, meta) : meta,
+    }
+  }
+
   actionCreator.TYPE = type
+
   return actionCreator
 
 }
+
+makeActionCreator.withDefaults = ({prefix = ACTION_PREFIX, defaultMeta}) => (name, argsMapper, options) => {
+  options = Object.assign({}, {prefix, defaultMeta}, options)
+  return makeActionCreator(name, argsMapper, options)
+}
+
 
 /**
  *
@@ -38,6 +75,11 @@ export function makeAsyncActionCreator(baseName, options) {
   actionCreator.failure = makeActionCreator(`${baseName}${ACTION_ASYNC_FAILURE_SUFFIX}`, options)
 
   return actionCreator
+}
+
+makeAsyncActionCreator.withDefaults = ({prefix = ACTION_PREFIX, defaultMeta}) => (name, argsMapper, options) => {
+  options = Object.assign({}, {prefix, defaultMeta}, options)
+  return makeAsyncActionCreator(name, argsMapper, options)
 }
 
 /**
@@ -58,7 +100,7 @@ export function makeAsyncReducer(actionCreator, options) {
     switch (type) {
       case actionCreator.TYPE:
         return options.shouldSpread ?
-          {loading: true, ...(options.destoryData && options.defaultData || {})} :
+          {loading: true, ...(options.defaultData || {})} :
           {loading: true, [options.dataProp]: options.shouldDestroyData ? options.defaultData : state.data}
       case actionCreator.success.TYPE:
         return options.shouldSpread ?
@@ -70,4 +112,9 @@ export function makeAsyncReducer(actionCreator, options) {
         return state
     }
   }
+}
+
+makeAsyncReducer.withDefaults = defaults => (actionCreator, options) => {
+  options = Object.assign({}, defaults, options)
+  return makeAsyncReducer(actionCreator, options)
 }
