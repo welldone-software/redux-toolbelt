7155e4f65f03e52f5799748fc71a470e43fb7a61
Arik Maor <arikmaor@gmail.com>
2017-06-18 15:08:36 +0300

    Adding unshiftItems

--- tests/immutableHelpers.js
+++ tests/immutableHelpers.js
@@ -1,11 +1,20 @@
-import {pushItems} from '../lib/immutableHelpers'
+import {pushItems, unshiftItems} from '../lib/immutableHelpers'
 
 import test from 'ava'
 
-test('push items', t => {
+test('pushItems', t => {
   const arr = [{a: 1}, {b: 2}]
   const result = pushItems(arr, [{c: 3}, {d: 4}])
   const expected = [{a: 1}, {b: 2}, {c: 3}, {d: 4}]
+
+  t.false(arr === result)
+  t.deepEqual(result, expected)
+})
+
+test('unshiftItems', t => {
+  const arr = [{a: 1}, {b: 2}]
+  const result = unshiftItems(arr, [{c: 3}, {d: 4}])
+  const expected = [{c: 3}, {d: 4}, {a: 1}, {b: 2}]
 
   t.false(arr === result)
   t.deepEqual(result, expected)
