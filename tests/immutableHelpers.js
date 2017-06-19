92370753db43cbd3271771c030790a3747cbaf86
Arik Maor <arikmaor@gmail.com>
2017-06-19 11:01:22 +0300

    Adding updateItemById

--- tests/immutableHelpers.js
+++ tests/immutableHelpers.js
@@ -1,4 +1,4 @@
-import {pushItems, unshiftItems, addItemsInIndex, removeItem, removeItemsById, replaceItem, repositionItem, repositionItemById, updateItem} from '../lib/immutableHelpers'
+import {pushItems, unshiftItems, addItemsInIndex, removeItem, removeItemsById, replaceItem, repositionItem, repositionItemById, updateItem, updateItemById} from '../lib/immutableHelpers'
 
 import test from 'ava'
 
@@ -96,6 +96,24 @@ test('updateItem', t => {
   const arr = [{id: 1, val: 3}, {id: 2, val: 5}, {id: 3, val: 8}]
   const result = updateItem(arr, 1, {val: 10, otherVal: 16})
   const expected = [{id: 1, val: 3}, {id: 2, val: 10, otherVal: 16}, {id: 3, val: 8}]
+
+  t.false(arr === result)
+  t.deepEqual(result, expected)
+})
+
+test('updateItemById', t => {
+  const arr = [{id: 1, val: 3}, {id: 2, val: 5}, {id: 3, val: 8}]
+  const result = updateItemById(arr, 2, {val: 10, otherVal: 16})
+  const expected = [{id: 1, val: 3}, {id: 2, val: 10, otherVal: 16}, {id: 3, val: 8}]
+
+  t.false(arr === result)
+  t.deepEqual(result, expected)
+})
+
+test('updateItemById with updating function', t => {
+  const arr = [{id: 1, val: 3}, {id: 2, val: 5}, {id: 3, val: 8}]
+  const result = updateItemById(arr, 2, item => ({val: item.val+1}))
+  const expected = [{id: 1, val: 3}, {id: 2, val: 6}, {id: 3, val: 8}]
 
   t.false(arr === result)
   t.deepEqual(result, expected)
