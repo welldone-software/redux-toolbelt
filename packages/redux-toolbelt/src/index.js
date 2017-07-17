5f690fd40dc4a9c39b5787f6ab0e5588e0d21925
Dani Kenan <danikenan@gmail.com>
2017-07-17 07:10:24 +0300

    export makeReducer

--- packages/redux-toolbelt/src/index.js
+++ packages/redux-toolbelt/src/index.js
@@ -1,6 +1,8 @@
 export composeReducers from './composeReducers'
 export makeActionCreator from './makeActionCreator'
+export makeReducer from './makeReducer'
 
 export makeAsyncActionCreator from './makeAsyncActionCreator'
 export * from './makeAsyncActionCreator'
 export makeAsyncReducer from './makeAsyncReducer'
+
