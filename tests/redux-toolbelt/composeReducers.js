c130762b4fb3c815921d30768974be8e64df65e4
vzaidman <vzaidman@gmail.com>
2017-09-01 11:43:53 +0300

    improved compose reducing to be able to be supplied only with a mapping function

--- tests/redux-toolbelt/composeReducers.js
+++ tests/redux-toolbelt/composeReducers.js
@@ -1,4 +1,4 @@
-import {composeReducers} from '../../packages/redux-toolbelt/src'
+import {composeReducers, makeReducer, makeActionCreator} from '../../packages/redux-toolbelt/src'
 
 import test from 'ava'
 
@@ -36,6 +36,34 @@ test('default state not applied when value supplied', t => {
 test('default state is applied when not supplied', t => {
   [r1r2, r2r1].forEach((f) => {
     t.is(f(undefined, action), 33 + f())
+  })
+})
+
+test('compose reducers made by makeReducers with only mapping', t => {
+  const setUserName = makeActionCreator('SET_USER_NAME')
+  const toggleShow = makeActionCreator('TOGGLE_SHOW')
+
+  const reducer = composeReducers({
+    userName: makeReducer(setUserName),
+    show: makeReducer(toggleShow, state => !state, {defaultState: true}),
+  })
+
+  const initialState = reducer(undefined, {type: '@@redux/INIT'})
+  t.deepEqual(initialState, {
+    userName: null,
+    show: true,
+  })
+
+  const state1 = reducer(initialState, setUserName('test-user-name'))
+  t.deepEqual(state1, {
+    userName: 'test-user-name',
+    show: true,
+  })
+
+  const state2 = reducer(state1, toggleShow())
+  t.deepEqual(state2, {
+    userName: 'test-user-name',
+    show: false,
   })
 })
 
