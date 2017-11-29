49c184d01c87c4ff0e4aa977568c82dbbb92d6c5
Dani Kenan <danikenan@gmail.com>
2017-11-29 08:28:55 +0200

    linter

--- packages/redux-toolbelt/src/makeAsyncReducer.js
+++ packages/redux-toolbelt/src/makeAsyncReducer.js
@@ -40,11 +40,11 @@ export default function makeAsyncReducer(actionCreator, options) {
       }
       case actionCreator.progress.TYPE:
         return {...state, progress: payload}
-      case actionCreator.failure.TYPE:  
-        return { 
-          ...(options.shouldDestroyDataOnError ? {} : state), 
-          loading: false, 
-          error: options.shouldSetError ? payload : undefined 
+      case actionCreator.failure.TYPE:
+        return {
+          ...(options.shouldDestroyDataOnError ? {} : state),
+          loading: false,
+          error: options.shouldSetError ? payload : undefined,
         }
       default:
         return state
