50627ea356efdd717fde03ae8a6a2e436fd09095
Giora Guttsait <giora111@gmail.com>
2018-01-06 12:27:16 +0200

    Add test for adding without updating

--- packages/redux-toolbelt-immutable-helpers/test/upsertItemsById.js
+++ packages/redux-toolbelt-immutable-helpers/test/upsertItemsById.js
@@ -8,8 +8,40 @@ test('upsertItemsById', () => {
     {id: 4, val: 1},
   ])
 
-  const result = upsertItemsById(arr, [{id: 2, val: 6}, {id: 3, val: 9}, {id: 5, val: 16}])
-  const expected = [{id: 1, val: 2}, {id: 2, val: 6}, {id: 3, val: 9}, {id: 4, val: 1}, {id: 5, val: 16}]
+  const result = upsertItemsById(arr, [
+    {id: 2, val: 6},
+    {id: 3, val: 9},
+    {id: 5, val: 16},
+  ])
+  const expected = [
+    {id: 1, val: 2},
+    {id: 2, val: 6},
+    {id: 3, val: 9},
+    {id: 4, val: 1},
+    {id: 5, val: 16},
+  ]
+
+  expect(result).toEqual(expected)
+})
+
+test('upsertItemsById add items without updates', () => {
+  const arr = Object.freeze([
+    {id: 1, val: 2},
+    {id: 2, val: 5},
+    {id: 3, val: 8},
+    {id: 4, val: 1},
+  ])
+
+  const result = upsertItemsById(arr, [
+    {id: 5, val: 16},
+  ])
+  const expected = [
+    {id: 1, val: 2},
+    {id: 2, val: 5},
+    {id: 3, val: 8},
+    {id: 4, val: 1},
+    {id: 5, val: 16},
+  ]
 
   expect(result).toEqual(expected)
 })
