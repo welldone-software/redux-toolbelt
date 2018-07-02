11a12b71e7ba398f2df6494f839e4a2b099c2e2f
Shahaf Shaked <shahafs@welldone-software.com>
2018-07-02 17:49:27 +0300

    Upgrade redux-observable to RxJS 6

--- packages/redux-toolbelt-observable/src/makeAsyncEpic.js
+++ packages/redux-toolbelt-observable/src/makeAsyncEpic.js
@@ -1,27 +1,27 @@
-import { Observable } from 'rxjs/Observable'
-import 'rxjs/add/observable/from'
-import 'rxjs/add/observable/of'
-import 'rxjs/add/observable/throw'
-import 'rxjs/add/operator/map'
-import 'rxjs/add/operator/catch'
-import 'rxjs/add/operator/switchMap'
+import { from, throwError, of } from 'rxjs'
+import { ofType } from 'redux-observable'
+import { mergeMap, map, catchError } from 'rxjs/operators'
 
 export default function makeAsyncEpic(actionCreator, asyncFn) {
   return (action$, {getState, dispatch}) =>
-    action$.ofType(actionCreator.TYPE)
-      .switchMap(action => {
+    action$.pipe(
+      ofType(actionCreator.TYPE),
+      mergeMap(action => {
         let obs = undefined
         try{
           obs = asyncFn(action.payload, action.type, action.meta, getState, dispatch)
         }
         catch(e){
-          obs = Observable.throw(e)
+          obs = throwError(e)
         }
         if (!obs || !obs.subscribe) {
-          obs = Observable.from(obs) //auto detect and convert to observable
+          obs = from(obs) //auto detect and convert to observable
         }
-        return obs
-          .map(payload => actionCreator.success(payload, action.meta))
-          .catch(error => Observable.of(actionCreator.failure(error, action.meta)))
-      })
+
+        return obs.pipe(
+          map(payload => actionCreator.success(payload, action.meta)),
+          catchError(error => of(actionCreator.failure(error, action.meta))),
+        )
+      }),
+    )
 }
