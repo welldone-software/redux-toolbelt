77d48e67b3d20a9d633b16524a1416462f9a27d6
nir-welldone <32127792+nir-welldone@users.noreply.github.com>
2018-12-25 10:56:13 +0200

    change name to cancelPreviousFunctionCalls & add _toolbeltAsyncFnArgs

--- packages/redux-toolbelt-observable/src/makeAsyncEpic.js
+++ packages/redux-toolbelt-observable/src/makeAsyncEpic.js
@@ -2,8 +2,8 @@ import { from, throwError, of } from 'rxjs'
 import { ofType } from 'redux-observable'
 import { mergeMap, switchMap, map, catchError } from 'rxjs/operators'
 
-export default function makeAsyncEpic(actionCreator, asyncFn, cancelPreviousRequests = false) {
-  const mapFunction = cancelPreviousRequests ? switchMap : mergeMap;
+export default function makeAsyncEpic(actionCreator, asyncFn, {cancelPreviousFunctionCalls = false} = {}) {
+  const mapFunction = cancelPreviousFunctionCalls ? switchMap : mergeMap
   return (action$, state$) =>
     action$.pipe(
       ofType(actionCreator.TYPE),
@@ -19,9 +19,12 @@ export default function makeAsyncEpic(actionCreator, asyncFn, cancelPreviousRequ
           obs = from(obs) //auto detect and convert to observable
         }
 
+        const { payload, meta: origMeta = {} } = action
+        const meta = { ...origMeta, _toolbeltAsyncFnArgs: payload }
+
         return obs.pipe(
-          map(payload => actionCreator.success(payload, action.meta)),
-          catchError(error => of(actionCreator.failure(error, action.meta))),
+          map(payload => actionCreator.success(payload, meta)),
+          catchError(error => of(actionCreator.failure(error, meta))),
         )
       }),
     )
--- packages/redux-toolbelt-observable/test/makeAsyncEpic.js
+++ packages/redux-toolbelt-observable/test/makeAsyncEpic.js
@@ -7,8 +7,8 @@ import {createEpicMiddleware} from 'redux-observable'
 
 const actionCreator = makeAsyncActionCreator('FETCH')
 
-const createStoreForTest = (asyncFunc, cancelPreviousRequests = false) => {
-  const epic = makeAsyncEpic(actionCreator, asyncFunc, cancelPreviousRequests)
+const createStoreForTest = (asyncFunc, options) => {
+  const epic = makeAsyncEpic(actionCreator, asyncFunc, options)
   const epicMiddleware = createEpicMiddleware()
   const createMockStore = configureMockStore([epicMiddleware])
   const store = createMockStore()
@@ -20,11 +20,13 @@ test('Dispatches success action for resolved promise', done => {
   const store = createStoreForTest(() => Promise.resolve(5))
   store.dispatch(actionCreator())
 
+  const actions = store.getActions()
+
   setImmediate(() => {
-    expect(store.getActions()).toEqual([
-      actionCreator(),
-      actionCreator.success(5),
-    ])
+    expect(actions[0].type).toBe(actionCreator.TYPE)
+
+    expect(actions[1].type).toBe(actionCreator.success.TYPE)
+    expect(actions[1].payload).toBe(5)
 
     done()
   })
@@ -34,66 +36,110 @@ test('Dispatches failure action for rejected promise', done => {
   const store = createStoreForTest(() => Promise.reject(7))
   store.dispatch(actionCreator())
 
+  const actions = store.getActions()
+
   setImmediate(() => {
-    expect(store.getActions()).toEqual([
-      actionCreator(),
-      actionCreator.failure(7),
-    ])
+    expect(actions[0].type).toBe(actionCreator.TYPE)
+
+    expect(actions[1].type).toBe(actionCreator.failure.TYPE)
+    expect(actions[1].payload).toBe(7)
+
     done()
   })
 })
 
-test('Dispatches failure action for exceptions', () => {
+test('Dispatches failure action for exceptions', done => {
   const error = new Error('Error object for test')
   const store = createStoreForTest(() => { throw error })
   store.dispatch(actionCreator())
 
-  expect(store.getActions()).toEqual([
-    actionCreator(),
-    actionCreator.failure(error),
-  ])
+  const actions = store.getActions()
+
+  setImmediate(() => {
+    expect(actions[0].type).toBe(actionCreator.TYPE)
+
+    expect(actions[1].type).toBe(actionCreator.failure.TYPE)
+    expect(actions[1].payload).toEqual(error)
+
+    done()
+  })
 })
 
-test('Dispatches success actions for observables', () => {
+test('Dispatches success actions for observables', done => {
   const store = createStoreForTest(() => from([3, 5, 7]))
   store.dispatch(actionCreator())
 
-  expect(store.getActions()).toEqual([
-    actionCreator(),
-    actionCreator.success(3),
-    actionCreator.success(5),
-    actionCreator.success(7),
-  ])
+  const actions = store.getActions()
+
+  setImmediate(() => {
+    expect(actions[0].type).toBe(actionCreator.TYPE)
+
+    expect(actions[1].type).toBe(actionCreator.success.TYPE)
+    expect(actions[1].payload).toBe(3)
+
+    expect(actions[2].type).toBe(actionCreator.success.TYPE)
+    expect(actions[2].payload).toBe(5)
+
+    expect(actions[3].type).toBe(actionCreator.success.TYPE)
+    expect(actions[3].payload).toBe(7)
+
+    expect(actions).toHaveLength(4)
+
+    done()
+  })
 })
 
 test('Dispatches few success actions for resolved promises', done => {
   const store = createStoreForTest(() => Promise.resolve(5))
-  store.dispatch(actionCreator())
-  store.dispatch(actionCreator())
+  store.dispatch(actionCreator({a: true}))
+  store.dispatch(actionCreator({b: true}))
+
+  const actions = store.getActions()
 
   setImmediate(() => {
-    expect(store.getActions()).toEqual([
-      actionCreator(),
-      actionCreator(),
-      actionCreator.success(5),
-      actionCreator.success(5),
-    ])
+    expect(actions[0].type).toBe(actionCreator.TYPE)
+    expect(actions[0].payload).toEqual({a: true})
+    expect(actions[0].meta).toBe(undefined)
+
+    expect(actions[1].type).toBe(actionCreator.TYPE)
+    expect(actions[1].payload).toEqual({b: true})
+    expect(actions[1].meta).toBe(undefined)
+
+    expect(actions[2].type).toBe(actionCreator.success.TYPE)
+    expect(actions[2].payload).toBe(5)
+    expect(actions[2].meta).toEqual({_toolbeltAsyncFnArgs: {a: true}})
+
+    expect(actions[3].type).toBe(actionCreator.success.TYPE)
+    expect(actions[3].payload).toBe(5)
+    expect(actions[3].meta).toEqual({_toolbeltAsyncFnArgs: {b: true}})
+
+    expect(actions).toHaveLength(4)
 
     done()
   })
 })
 
 test('Dispatches few success actions for resolved promises and cancel previous requests', done => {
-  const store = createStoreForTest(() => Promise.resolve(5), true)
-  store.dispatch(actionCreator())
-  store.dispatch(actionCreator())
+  const store = createStoreForTest(() => Promise.resolve(5), {cancelPreviousFunctionCalls: true})
+  store.dispatch(actionCreator({a: true}))
+  store.dispatch(actionCreator({b: true}))
+
+  const actions = store.getActions()
 
   setImmediate(() => {
-    expect(store.getActions()).toEqual([
-      actionCreator(),
-      actionCreator(),
-      actionCreator.success(5),
-    ])
+    expect(actions[0].type).toBe(actionCreator.TYPE)
+    expect(actions[0].payload).toEqual({a: true})
+    expect(actions[0].meta).toBe(undefined)
+
+    expect(actions[1].type).toBe(actionCreator.TYPE)
+    expect(actions[1].payload).toEqual({b: true})
+    expect(actions[1].meta).toBe(undefined)
+
+    expect(actions[2].type).toBe(actionCreator.success.TYPE)
+    expect(actions[2].payload).toBe(5)
+    expect(actions[2].meta).toEqual({_toolbeltAsyncFnArgs: {b: true}})
+
+    expect(actions).toHaveLength(3)
 
     done()
   })
